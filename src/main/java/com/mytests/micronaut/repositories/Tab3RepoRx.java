6d6352dae9e113ba964ea77a87fe6515903e4682
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-09-08 18:40:08 +0300

    formatting

--- src/main/java/com/mytests/micronaut/repositories/Tab3RepoRx.java
+++ src/main/java/com/mytests/micronaut/repositories/Tab3RepoRx.java
@@ -15,7 +15,6 @@ public interface Tab3RepoRx extends RxJavaCrudRepository<Tab3,Integer> {
     Flowable<Tab3> findByFirstContains(String first);
     
     Single<Tab3> findByThird(int third);
-
     
     Maybe<Tab3> findByThirdAndSecond(int third, String second);
 }
